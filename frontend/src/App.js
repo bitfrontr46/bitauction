f2df33c6f60b1d7a09f8c2ea8a35889041ec1f23
chaarlesjeong <chaarlesjeong@gmail.com>
2020-08-06 17:44:35 +0900

    jch-Routes통일화&각 페이지에 Footer, Navigation 추가(App 복귀)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Routes from "./routes/Routes";
 
 //css reset 참고하기.
 function App() {
+  //Login Session 유지
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const is_login = () => {
+      if (localStorage.getItem("is_login") === "true") {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    const is_seller = () => {
+      if (localStorage.getItem("is_seller") === "true") {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    const user_id = localStorage.getItem("user_id");
+    const userName = localStorage.getItem("userName");
+    if (is_login()) {
+      console.log("자동로그인!");
+      dispatch({
+        type: "LOGIN",
+        payload: {
+          user_id: user_id,
+          is_seller: is_seller(),
+          userName: userName,
+        },
+      });
+    }
+  }, [dispatch]);
   return (
     <>
       <Routes />
